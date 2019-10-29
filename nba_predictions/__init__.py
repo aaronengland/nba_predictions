dc880d082ebac4017eb15a19145535ab47abac6c
Aaron England <aaron.england24@gmail.com>
2019-10-29 16:14:45 -0700

    Update __init__.py

--- nba_predictions/__init__.py
+++ nba_predictions/__init__.py
@@ -239,200 +239,81 @@ def tune_hyperparameters(df, list_outer_weighted_mean, list_distributions, list_
     # return dict_results
     return dict_results
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# define function for nba season simulation
-def nba_season_simulation(year, weighted_mean=False, n_simulations=1000):
-    # create list of momths
-    list_months = ['october','november','december','jauary','february','march','april','may','june']
-    # instantiate lists
-    list_month = []
-    list_away_team = []
-    list_away_score = []
-    list_home_team = []
-    list_home_score = []
-    # get rows for each momth
-    for month in list_months:
-        # get url
-        r = requests.get('https://www.basketball-reference.com/leagues/NBA_{0}_games-{1}.html'.format(year, month))
-        # get content of page
-        soup = BeautifulSoup(r.content, 'html.parser')
-        # get the schedule table
-        table = soup.find('table', id='schedule')
-        if table:
-            # get all table rows
-            table_rows = table.find_all('tr')
-            # get columns
-            for row in table_rows:
-                # get columns (i.e., td)
-                columns = row.find_all('td')
-                # if columns is not an empty list
-                if columns:
-                    # append month to list_month
-                    list_month.append(month)
-                    # get away team
-                    away_team = columns[1].text
-                    # append to list_away_team
-                    list_away_team.append(away_team)
-                    # get away_score
-                    away_score = columns[2].text
-                    # append to list_away_score
-                    list_away_score.append(away_score)
-                    # get home team
-                    home_team = columns[3].text
-                    # append to list_home_team
-                    list_home_team.append(home_team)
-                    # get home score
-                    home_score = columns[4].text
-                    # append to list_home_score
-                    list_home_score.append(home_score)
-            
-    # put into df
-    df = pd.DataFrame({'month': list_month,
-                       'home_team': list_home_team,
-                       'away_team': list_away_team,
-                       'home_points': list_home_score,
-                       'away_points': list_away_score})
-    
-    # replace '' with nan
-    for column in list(df.columns):
-        df[column] = df.apply(lambda x: np.nan if x[column] == '' else x[column], axis=1)
-    
-    # convert score cols to integer
-    for column in list(df.columns)[3:]:
-        df[column] = df.apply(lambda x: x[column] if pd.isnull(x[column]) else int(x[column]), axis=1)
-
-    # get the played games
-    df_played_games = df.dropna(subset=['home_points'])    
+# simulate season
+def nba_season_simulation(df, dict_best_hyperparameters, n_simulations=1000):
+    # get winning team
+    df['winning_team'] = df.apply(lambda x: x['home_team'] if x['home_points'] > x['away_points'] else x['away_team'], axis=1)  
         
+    # drop the unplayed games
+    df_played = df.dropna(subset=['home_points'])
     # get the unplayed games
-    df_unplayed_games = df[df.isnull().any(axis=1)]
+    df_unplayed = df[pd.isnull(df['home_points'])]
     
-    # instantiate lists
-    list_home_score = []
-    list_away_score = []
-    list_home_win_prob = []
-    for i in range(df_unplayed_games.shape[0]):
-        # get home_team
-        home_team = df_unplayed_games['home_team'].iloc[i]
-        # get away_team
-        away_team = df_unplayed_games['away_team'].iloc[i]
+    # simulate each game with best hyperparameters
+    df_unplayed['pred_outcome'] = df_unplayed.apply(lambda x: game_predictions(home_team_array=df_played['home_team'], 
+                                                                               home_score_array=df_played['home_points'], 
+                                                                               away_team_array=df_played['away_team'], 
+                                                                               away_score_array=df_played['away_points'], 
+                                                                               home_team=x['home_team'], 
+                                                                               away_team=x['away_team'], 
+                                                                               outer_weighted_mean=dict_best_hyperparameters.get('outer_weighted_mean'), 
+                                                                               distribution=dict_best_hyperparameters.get('distribution'),
+                                                                               inner_weighted_mean=dict_best_hyperparameters.get('inner_weighted_mean'), 
+                                                                               weight_home=dict_best_hyperparameters.get('weight_home'),
+                                                                               weight_away=dict_best_hyperparameters.get('weight_away'),
+                                                                               n_simulations=n_simulations), axis=1)
     
-        # check to make sure each team is in the respective lists
-        if home_team in list(df_played_games['home_team']) and away_team in list(df_played_games['away_team']):
-            # simulate game
-            simulated_game = game_predictions(home_team_array=df_played_games['home_team'], 
-                                              home_score_array=df_played_games['home_points'], 
-                                              away_team_array=df_played_games['away_team'], 
-                                              away_score_array=df_played_games['away_points'], 
-                                              home_team=home_team, 
-                                              away_team=away_team,
-                                              n_simulations=n_simulations,
-                                              weighted_mean=weighted_mean)
-            # get the predicted home score
-            home_score = simulated_game.mean_home_score
-            # get the predicted away score
-            away_score = simulated_game.mean_away_score
-            # get the predicted win probability
-            home_win_prob = simulated_game.prop_home_win
-        else:
-            home_score = 'NA'
-            away_score = 'NA'
-            home_win_prob = 'NA'
-        # append to lists
-        list_home_score.append(home_score)
-        list_away_score.append(away_score)
-        list_home_win_prob.append(home_win_prob)
+    # put into df_unplayed
+    df_unplayed['home_points'] = df_unplayed.apply(lambda x: x['pred_outcome'].get('mean_home_pts'), axis=1)
+    df_unplayed['away_points'] = df_unplayed.apply(lambda x: x['pred_outcome'].get('mean_away_pts'), axis=1)
+    df_unplayed['winning_team'] = df_unplayed.apply(lambda x: x['pred_outcome'].get('winning_team'), axis=1)
     
-    # put into df_unplayed_games
-    df_unplayed_games['home_points'] = list_home_score
-    df_unplayed_games['away_points'] = list_away_score
-    df_unplayed_games['home_win_prob'] = list_home_win_prob
-
-    # choose the winning team
-    df_unplayed_games['winning_team'] = df_unplayed_games.apply(lambda x: x['home_team'] if x['home_points'] >= x['away_points'] else x['away_team'], axis=1)
-
-    # make columns in df_played_games match with df_unplayed_games
-    df_played_games['home_win_prob'] = df_played_games.apply(lambda x: 1.0 if x['home_points'] > x['away_points'] else 0.0, axis=1)
-    df_played_games['winning_team'] = df_played_games.apply(lambda x: x['home_team'] if x['home_points'] >= x['away_points'] else x['away_team'], axis=1)
-
-    # append df_unplayed_games to df_played_games
-    df_simulated_season = df_played_games.append(df_unplayed_games)
-
+    # drop pred_outcome
+    df_unplayed.drop(['pred_outcome'], axis=1, inplace=True)
+    
+    # append df_played and df_unplayed
+    df_final = pd.concat([df_played, df_unplayed],ignore_index=True)
+    
     # get number of wins for each team
-
-    # get unique teams
-    df_unique_teams = pd.DataFrame(pd.unique(df['home_team']))
-
-    # get the wins for each team
-    list_unique_winning_teams = list(pd.value_counts(df_simulated_season['winning_team']).index)
-    list_n_wins = list(pd.value_counts(df_simulated_season['winning_team']))
-
-    # put into a df
-    df_predicted_wins = pd.DataFrame({'team': list_unique_winning_teams,
-                                      'wins': list_n_wins})
-
-    # left join df_unique_teams and df_predicted_wins
-    df_final_win_predictions = pd.merge(left=df_unique_teams, right=df_predicted_wins,
-                                        left_on=df_unique_teams[0], right_on='team',
-                                        how='left').fillna(0)
+    df_unique_teams = pd.DataFrame({'team': pd.unique(df_final['home_team'])})
     
-    # drop the col we dont want
-    df_final_win_predictions.drop([0], axis=1, inplace=True)    
-
-    # get predicted losses
-    df_final_win_predictions['losses'] = 82 - df_final_win_predictions['wins']
-
-    # sort by wins
-    df_final_win_predictions = df_final_win_predictions.sort_values(by=['wins'], ascending=False)
+    # define function to get n_wins
+    def n_wins(team):
+        num_wins = 0
+        for winning_team in df_final['winning_team']:
+            if winning_team == team:
+                num_wins += 1
+        return num_wins
     
-    # get the conference for each team
+    # apply function for each team
+    df_unique_teams['n_wins'] = df_unique_teams.apply(lambda x: n_wins(team=x['team']), axis=1)
+    
+    # sort by n_wins
+    df_unique_teams = df_unique_teams.sort_values(by=['n_wins'], ascending=False)
+    
+    # get losses
+    df_unique_teams['n_losses'] = 82 - df_unique_teams['n_wins']
+    
+    # get each teams conference
     df_nba_teams_conferences = pd.read_csv('https://raw.githubusercontent.com/aaronengland/data/master/nba_teams_conferences.csv')
     
-    # left join df_final_win_predictions and df_nba_teams_conferences on team name
-    df_final_win_predictions_conf = pd.merge(left=df_final_win_predictions, right=df_nba_teams_conferences, 
-                                             left_on='team', right_on='Team',
-                                             how='left')
-
-    # drop the cols we don't need
-    df_final_win_predictions_conf.drop(['Unnamed: 0', 'ID', 'Team'], axis=1, inplace=True)
-
-    # separate into east and west
-    df_east = df_final_win_predictions_conf[df_final_win_predictions_conf['Conference'] == 'East']
-    df_west = df_final_win_predictions_conf[df_final_win_predictions_conf['Conference'] == 'West']
+    # join df_unique_teams and df_nba_teams_conferences
+    df_unique_teams_w_conf = pd.merge(left=df_unique_teams, right=df_nba_teams_conferences,
+                                      left_on='team', right_on='Team', how='left')
     
-    # define attributes class
-    class attributes:
-        def __init__(self, df_simulated_season, df_final_win_predictions_conf, df_east, df_west):
-            self.df_simulated_season = df_simulated_season
-            self.df_final_win_predictions_conf = df_final_win_predictions_conf
-            self.df_east = df_east
-            self.df_west = df_west
-    # save returnable object
-    x = attributes(df_simulated_season, df_final_win_predictions_conf, df_east, df_west)
-    return x
+    # get west
+    df_west = df_unique_teams_w_conf[df_unique_teams_w_conf['Conference'] == 'West'].reset_index(drop=True)
+    # get east
+    df_east = df_unique_teams_w_conf[df_unique_teams_w_conf['Conference'] == 'East'].reset_index(drop=True)
+    
+    # create dictionary for which to return
+    results = {'simulated_season': df_final,
+               'final_win_predictions': df_unique_teams_w_conf,
+               'west': df_west,
+               'east': df_east}
+    
+    # return results
+    return results
 
 
 
